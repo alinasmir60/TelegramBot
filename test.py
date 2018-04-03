8d49b47b000c8c051b3204bcedf17dacc93198ca
Alina Smirnova <alinasmir60@yandex.ru>
2018-04-03 19:27:52 +0300

    Реализован пререводчик

--- test.py
+++ test.py
@@ -27,8 +27,9 @@ def help(bot, update):
 
 
 def quiz(bot, update, user_data):
+    user_data["func"] = "quiz"
     choice_word = random.choice(word)
-    trans_word = [translater_word(choice_word)]
+    trans_word = [translater_word(choice_word, "ru-en")]
     while len(trans_word) <= 2:
         w = random.choice(translat)
         if w not in trans_word:
@@ -45,19 +46,23 @@ def quiz(bot, update, user_data):
 
 def answer(bot, update, user_data):
     answ = update.message.text
-    reply_keyboard_true = [["/Опрос"], ["/stop"]]
-    reply_keyboard_false = [["Повторить попытку", "Правильный ответ"], ["/stop"]]
-    print(answ)
-    if answ == user_data["true_answ"][1]:
-        update.message.reply_text("Правильно!", reply_markup=ReplyKeyboardMarkup(reply_keyboard_true))
-    elif answ == "Правильный ответ":
-        update.message.reply_text(answ, user_data["true_answ"][1], reply_markup=ReplyKeyboardMarkup(reply_keyboard_true))
-        quiz()
-    elif answ == "Повторить попытку":
-        update.message.reply_text("Вы решили повторить попытку.")
-        update.message.reply_text(user_data["true_answ"][0], reply_markup=user_data["keyboard"])
-    elif answ != user_data["true_answ"]:
-        update.message.reply_text("Не правильно!", reply_markup=ReplyKeyboardMarkup(reply_keyboard_false))
+    if user_data["func"] == "quiz":
+        reply_keyboard_true = [["/Опрос"], ["/stop"]]
+        reply_keyboard_false = [["Повторить попытку", "Правильный ответ"], ["/stop"]]
+        if answ == user_data["true_answ"][1]:
+            update.message.reply_text("Правильно!", reply_markup=ReplyKeyboardMarkup(reply_keyboard_true))
+        elif answ == "Правильный ответ":
+            update.message.reply_text("{}: {}".format(answ, user_data["true_answ"][0]))
+        elif answ == "Повторить попытку":
+            update.message.reply_text("Вы решили повторить попытку.")
+            update.message.reply_text(user_data["true_answ"][0], reply_markup=user_data["keyboard"])
+        elif answ != user_data["true_answ"]:
+            update.message.reply_text("Не правильно!", reply_markup=ReplyKeyboardMarkup(reply_keyboard_false))
+    elif user_data["func"] == "translate":
+        text = translater_word(answ, user_data["lang"])
+        accompanying_text = "Переведено сервисом «Яндекс.Переводчик» http://translate.yandex.ru/."
+        update.message.reply_text("\n\n".join([text, accompanying_text]))
+
 
 
 def en_ru(bot, update, user_data):
@@ -67,32 +72,34 @@ def en_ru(bot, update, user_data):
 
 def ru_en(bot, update, user_data):
     update.message.reply_text("Язык перевода: Русский - Английский")
+    user_data["lang"] = "ru-en"
 
 
-def translater_word(text):
+
+def translater_word(text, lang):
     translator_uri = "https://translate.yandex.net/api/v1.5/tr.json/translate"
     response = requests.get(
         translator_uri,
         params={
             "key":
             "trnsl.1.1.20180401T224213Z.c1143abeda1b8ddf.366d17dd4a20cb2ccc211c86c4bb1ec637a175de",
-            "lang": "ru-en",
+            "lang": lang,
             "text": text
         })
     return response.json()["text"][0]
 
 
-def translater(bot, update, args):
-    accompanying_text = "Переведено сервисом «Яндекс.Переводчик» http://translate.yandex.ru/."
-    text = translater_word(args)
-    update.message.reply_text("\n\n".join([text, accompanying_text]))
+def translater(bot, update, user_data):
+    user_data["func"] = "translate"
+    reply_keyboard_transl = [["/en_ru", "/ru_en"], ["/Опрос", "/stop"]]
+    update.message.reply_text("Вы активировали переводчик.\nВы можете выбрать язык перевода.", reply_markup=ReplyKeyboardMarkup(reply_keyboard_transl))
 
 
 reply_keyboard_start = [['/Опрос', '/Переводчик'], ["/stop"]]
 markup_start = ReplyKeyboardMarkup(reply_keyboard_start, one_time_keyboard=False)
 
 word = ["человек", "стол", "мама", "папа", "сестра"]
-translat = [translater_word(i) for i in word]
+translat = [translater_word(i, "ru-en") for i in word]
 
 
 def main():
@@ -103,7 +110,8 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("Опрос", quiz, pass_user_data=True))
     dp.add_handler(CommandHandler("en_ru", en_ru, pass_user_data=True))
-    dp.add_handler(CommandHandler("Переводчик", translater, pass_args=True))
+    dp.add_handler(CommandHandler("ru_en", ru_en, pass_user_data=True))
+    dp.add_handler(CommandHandler("Переводчик", translater, pass_user_data=True))
     dp.add_handler(CommandHandler("stop", stop))
     dp.add_handler(MessageHandler(Filters.text, answer, pass_user_data=True))
 
